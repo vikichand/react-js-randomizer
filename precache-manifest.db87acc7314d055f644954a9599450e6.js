8f7a4bf65c93980a41a939b51753def6c29c541e
vikichand <chand_vikash@ymail.com>
2020-04-11 12:57:55 +1200

    Updates

---
+++ precache-manifest.db87acc7314d055f644954a9599450e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009bd29fbbcf32ffba82e9ef2573320e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce3d3c2e388365590f3a",
+    "url": "./static/css/main.52dee186.chunk.css"
+  },
+  {
+    "revision": "489355ff4b0c3a9276d8",
+    "url": "./static/js/2.deae5766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.deae5766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3d3c2e388365590f3a",
+    "url": "./static/js/main.8f6eb8a3.chunk.js"
+  },
+  {
+    "revision": "7a83b6775a4703c14432",
+    "url": "./static/js/runtime-main.32dd003a.js"
+  }
+]);
